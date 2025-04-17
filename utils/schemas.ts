2ecee11b2d8ec664b151812198a6562b68ce8f97
Dzarek <jarekjanas95@gmail.com>
2025-04-18 00:54:17 +0200

    image on supabase

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -22,3 +22,22 @@ export const validateWithZodSchema = (schema: ZodSchema, data: unknown) => {
   }
   return result.data;
 };
+
+export const imageSchema = z.object({
+  image: validateFile(),
+});
+
+function validateFile() {
+  const maxUploadSize = 1024 * 1024;
+  const acceptedFilesTypes = ["image/"];
+  return z
+    .instanceof(File)
+    .refine((file) => {
+      return !file || file.size <= maxUploadSize;
+    }, "File size must be less than 1 MB")
+    .refine((file) => {
+      return (
+        !file || acceptedFilesTypes.some((type) => file.type.startsWith(type))
+      );
+    }, "File must be an image");
+}
